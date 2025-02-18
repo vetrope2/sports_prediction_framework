4a35122c475476b01e62b2d0b1f7f9dc5cfc9c20
vetrope2 <vetrope2@fel.cvut.cz>
2025-02-18 16:32:39 +0100

    SQLalchemy added and .env added

--- dataloader/Connector.py
+++ dataloader/Connector.py
@@ -1,33 +1,38 @@
 import paramiko
 from sshtunnel import SSHTunnelForwarder
 import psycopg2
+from dotenv import load_dotenv, dotenv_values
 from sqlalchemy import create_engine
 from logger import framework_logger
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+dotenv_path = os.path.join(BASE_DIR, "..", ".env")
+
 class Connector:
+    def __init__(self):
+        self.eng = None
+        self.config = dotenv_values(dotenv_path)
 
     def connect(self, config_file: str):
         pass
-    def connect_to_db_via_ssh(self, ssh_host, ssh_user, ssh_key_path, db_host, db_port, db_name, db_user, db_password):
+    def connect_to_db_via_ssh(self):
+
         try:
             # Establish SSH tunnel
+
             tunnel = SSHTunnelForwarder(
-                (ssh_host, 22),  # SSH server and port
-                ssh_username=ssh_user,
-                ssh_pkey=ssh_key_path,
-                remote_bind_address=(db_host, db_port),  # Remote database address
+                (self.config['SSH_HOST'], 22),  # SSH server and port
+                ssh_username=self.config['SSH_USER'],
+                ssh_pkey=self.config['SSH_KEY_PATH'],
+                remote_bind_address=(self.config['DB_HOST'], int(self.config['DB_PORT'])),  # Remote database address
                 local_bind_address=('127.0.0.1', 5433)  # Local forwarding
             )
+
             tunnel.start()
 
-            # Connect to PostgreSQL through the tunnel
-            conn = psycopg2.connect(
-                host="127.0.0.1",
-                port=5433,
-                dbname=db_name,
-                user=db_user,
-                password=db_password
-            )
+            self.eng = create_engine(f'postgresql+psycopg2://{self.config["DB_USER"]}:{self.config["DB_PASSWORD"]}@127.0.0.1:5433/{self.config["DB_NAME"]}')
             print("Connected")
-            return conn, tunnel
         except Exception as e:
-            return None, None
+            print("Problem")
+            pass
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from dataloader.Connector import Connector
+import pandas as pd
 
 ssh_host = "potato.felk.cvut.cz"  # Replace with actual SSH server
 ssh_user = "vetrope2"
@@ -11,18 +12,8 @@ db_user = 'oganetig'
 db_password = 'oganetig-fel-cvut-cz'
 
 c = Connector()
-conn, tunnel = c.connect_to_db_via_ssh(ssh_host, ssh_user, ssh_key_path, db_host, db_port, db_name, db_user, db_password)
+c.connect_to_db_via_ssh()
 
-cursor = conn.cursor()
-cursor.execute('SELECT * FROM "isdb"."Leagues" LIMIT 5;')  # Query the League table
-rows = cursor.fetchall()
 
-# Print column names
-colnames = [desc[0] for desc in cursor.description]
-print(f"Columns: {colnames}")
-
-# Print rows
-for row in rows:
-    print(row)
-
-cursor.close()
+frame = pd.read_sql_query('SELECT * FROM "isdb"."Leagues" LIMIT 5;', con=c.eng)
+print(frame)
