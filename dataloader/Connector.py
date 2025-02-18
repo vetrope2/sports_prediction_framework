e25269a0cc1ef610c9710a9b91e96d07d37e9420
vetrope2 <vetrope2@fel.cvut.cz>
2025-02-18 16:47:45 +0100

    Logger problem fixed

--- dataloader/Connector.py
+++ dataloader/Connector.py
@@ -11,17 +11,21 @@ dotenv_path = os.path.join(BASE_DIR, "..", ".env")
 
 class Connector:
     def __init__(self):
-        self.eng = None
+
         self.config = dotenv_values(dotenv_path)
 
-    def connect(self, config_file: str):
-        pass
+    def connect_to_db(self):
+        try:
+            self.eng = create_engine(f'postgresql+psycopg2://{self.config["DB_USER"]}:{self.config["DB_PASSWORD"]}@127.0.0.1:5433/{self.config["DB_NAME"]}')
+            framework_logger.info("Connected to database")
+        except Exception as e:
+            framework_logger.error("Connection to database failed.")
     def connect_to_db_via_ssh(self):
 
         try:
             # Establish SSH tunnel
 
-            tunnel = SSHTunnelForwarder(
+            self.tunnel = SSHTunnelForwarder(
                 (self.config['SSH_HOST'], 22),  # SSH server and port
                 ssh_username=self.config['SSH_USER'],
                 ssh_pkey=self.config['SSH_KEY_PATH'],
@@ -29,10 +33,11 @@ class Connector:
                 local_bind_address=('127.0.0.1', 5433)  # Local forwarding
             )
 
-            tunnel.start()
+            self.tunnel.start()
 
-            self.eng = create_engine(f'postgresql+psycopg2://{self.config["DB_USER"]}:{self.config["DB_PASSWORD"]}@127.0.0.1:5433/{self.config["DB_NAME"]}')
-            print("Connected")
+            self.connect_to_db()
         except Exception as e:
-            print("Problem")
             pass
+
+    def close(self):
+        self.tunnel.stop()
--- test.py
+++ test.py
@@ -1,15 +1,6 @@
 from dataloader.Connector import Connector
 import pandas as pd
 
-ssh_host = "potato.felk.cvut.cz"  # Replace with actual SSH server
-ssh_user = "vetrope2"
-ssh_key_path = "~/.ssh/id_rsa"  # Replace with your SSH private key path
-
-db_host = "147.32.83.171"  # PostgreSQL server's actual IP
-db_port = 5432
-db_name = "bet"
-db_user = 'oganetig'
-db_password = 'oganetig-fel-cvut-cz'
 
 c = Connector()
 c.connect_to_db_via_ssh()
