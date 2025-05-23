8151bd4a12eb915ba8d515871ad3e3f01f23193a
vetrope2 <vetrope2@fel.cvut.cz>
2025-05-23 08:22:11 +0200

    Sport enum extended

--- sports_prediction_framework/datawrapper/SportType.py
+++ sports_prediction_framework/datawrapper/SportType.py
@@ -1,13 +1,19 @@
 from enum import Enum
 from sports_prediction_framework.dataloader.parser.MatchParser import MatchParser
-from sports_prediction_framework.datawrapper.sport.match.FootballWrapper import FootballWrapper
+from sports_prediction_framework.datawrapper.sport.match import FootballWrapper, AmericanFootballWrapper,BaseballWrapper,BasketballWrapper, HockeyWrapper
 from sports_prediction_framework.dataloader.parser.RaceParser import RaceParser
-from sports_prediction_framework.datawrapper.sport.race.GolfWrapper import GolfWrapper
+from sports_prediction_framework.datawrapper.sport.race import GolfWrapper, CyclingWrapper, FormulaOneWrapper
 
 
 class SportType(Enum):
     FOOTBALL = (MatchParser, FootballWrapper)
     GOLF = (RaceParser, GolfWrapper)
+    AMERICAN_FOOTBALL = (MatchParser, AmericanFootballWrapper)
+    BASEBALL = (MatchParser, BaseballWrapper)
+    BASKETBALL = (MatchParser, BasketballWrapper)
+    HOCKEY = (MatchParser, HockeyWrapper)
+    CYCLING = (RaceParser, CyclingWrapper)
+    FORMULA_ONE = (RaceParser, FormulaOneWrapper)
 
 
     def get_parser(self):
