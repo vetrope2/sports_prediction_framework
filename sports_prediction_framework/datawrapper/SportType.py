baec1955be77af53a590a0ba668ad124d566014d
vetrope2 <vetrope2@fel.cvut.cz>
2025-05-29 09:23:41 +0200

    Module import fixed

--- sports_prediction_framework/datawrapper/SportType.py
+++ sports_prediction_framework/datawrapper/SportType.py
@@ -1,8 +1,14 @@
 from enum import Enum
 from sports_prediction_framework.dataloader.parser.MatchParser import MatchParser
-from sports_prediction_framework.datawrapper.sport.match import FootballWrapper, AmericanFootballWrapper,BaseballWrapper,BasketballWrapper, HockeyWrapper
+from sports_prediction_framework.datawrapper.sport.match.FootballWrapper import FootballWrapper
+from sports_prediction_framework.datawrapper.sport.match.AmericanFootballWrapper import AmericanFootballWrapper
+from sports_prediction_framework.datawrapper.sport.match.BaseballWrapper import BaseballWrapper
+from sports_prediction_framework.datawrapper.sport.match.BasketballWrapper import BasketballWrapper
+from sports_prediction_framework.datawrapper.sport.match.HockeyWrapper import HockeyWrapper
 from sports_prediction_framework.dataloader.parser.RaceParser import RaceParser
-from sports_prediction_framework.datawrapper.sport.race import GolfWrapper, CyclingWrapper, FormulaOneWrapper
+from sports_prediction_framework.datawrapper.sport.race.GolfWrapper import GolfWrapper
+from sports_prediction_framework.datawrapper.sport.race.CyclingWrapper import  CyclingWrapper
+from sports_prediction_framework.datawrapper.sport.race.FormulaOneWrapper import FormulaOneWrapper
 
 
 class SportType(Enum):
